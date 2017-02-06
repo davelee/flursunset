9a381b58ddb2f785933ce0a46fe30db58c92a273
Dave Lee <davmlee@umich.edu>
2017-02-06 03:42:56 -0800

    share to fb link feature

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -3,22 +3,35 @@
 	// Reference the flur-app module
 	angular.module('flur-app', [])
 
-	.controller('PhotoController', PhotoController);
+	.controller('PhotoController', PhotoController)
 
-	function PhotoController ($scope) {
+  .filter('fbShareFilter', function () {
+    return function (path) {
+      return "https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdavelee.io%2Fflur%23photo-box%3FshareId%3D" + path + "&amp;src=sdkpreparse";
+    };
+  });
+
+	function PhotoController ($scope, $location) {
 
     var vm = this;
+
+    var share_id = $location.search().shareId;
 
     vm.goToPhotos = function () {
         console.log("hello");
       	$('html,body').animate({scrollTop: $("#photo-box").offset().top},'slow');
     };
 
-		vm.getAllPhotos = function () {
+		vm.getAllPhotos = function (share_id) {
       var photos = [];
+      var once = true;
+      console.log(share_id);
       for (var i = 1; i < 5; i++) {
-
         var r = Math.floor((Math.random() * 817) + 1);
+        if (share_id != null && once == true) {
+          r = share_id.substr(0,4);
+          once = false;
+        }
         var flurImage = {};
         var path = ("0000" + r).substr(-4,4) + ".gif";
         flurImage.url = "https://flur.davelee.io/photos/" + ("0000" + r).substr(-4,4) + ".gif";
@@ -33,6 +46,6 @@
       vm.photos = photos;
 		};
 
-    vm.getAllPhotos();
+    vm.getAllPhotos(share_id);
 	}
 })();
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -5,3 +5,9 @@ function goToByScroll(id){
 $("#sunset-box").click(function() {
   goToByScroll('photo-box');
 });
+
+$(window).scroll(function(){
+    $("#flur-icon-container").css("opacity", 1 - $(window).scrollTop() / 300);
+    $("#thankyou-text").css("opacity", 1 - $(window).scrollTop() / 400);
+    $("footer").css("opacity", 1 - $(window).scrollTop() / 500);
+});
