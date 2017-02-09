dacc633757ec0026f56d5ef617325ec30e368e67
Dave Lee <davmlee@umich.edu>
2017-02-08 22:33:55 -0800

    last update with endpoint for smaller thumbnails

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -37,7 +37,7 @@
         var flurImage = {};
         var path = ("0000" + r).substr(-4,4) + ".gif";
         flurImage.url = "https://flur.davelee.io/photos/" + ("0000" + r).substr(-4,4) + ".gif";
-        flurImage.url_thumb = "https://flur.davelee.io/thumbnails/" + ("0000" + r).substr(-4,4) + ".jpg";
+        flurImage.url_thumb = "https://flur.davelee.io/thumbs/" + ("0000" + r).substr(-4,4) + ".jpg";
 
         var photo = {};
         photo.image = flurImage;
